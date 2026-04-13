1ccb0f49b5a8b60411d5c1a97bcd6e6d2c9b123b
Abderehman Ali <ali.abderehman4@gmail.com>
2026-04-13 12:31:28 +0300

    fix: error loading

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,9 @@ import { getPrayerTime } from "../scripts/prayerTime";
 
 const template = document.getElementById("time-box");
 const timeContainer = document.querySelector(".time-box-container");
+const hijriDate = document.querySelector(".hijri-date");
+const enDate = document.querySelector(".en-date");
+const locationName = document.querySelector(".location-name");
 
 async function getLocation() {
   return new Promise((resolve, reject) => {
@@ -13,11 +16,12 @@ async function init() {
   try {
     const position = await getLocation();
 
-    const { latitude, longitude } = await position.coords;
+    const { latitude, longitude } = position.coords;
 
     const prayerData = await getPrayerTime(latitude, longitude);
     console.log(prayerData);
-    updateUI(prayerData);
+
+    updateUI(prayerData, { latitude, longitude });
   } catch (e) {
     console.error(e);
   }
@@ -38,29 +42,62 @@ function formatTime(time24) {
   };
 }
 
-function updateUI(data) {
+function updateUI(data, coords) {
   const name = Object.keys(data.data.timings).slice(0, 7);
 
-  console.log(name);
-  const time = Object.values(data.data.timings);
+  const prayerNames = ["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"];
+
+  // console.log(name);
+  // const time = Object.values(data.data.timings);
   //   cont time
 
-  name.forEach((na, i, _) => {
+  prayerNames.forEach((name) => {
     // console.log(i);
-    renderPrayerTime(na, formatTime(time[i]));
+    const time = data.data.timings[name];
+    renderPrayerTime(name, formatTime(time));
   });
+
+  updateDate(data.data.date.hijri, data.data.date.readable);
+  renderLocationName(coords);
 }
 
-function renderPrayerTime(name, time, period = "AM") {
+function renderPrayerTime(name, time) {
   const clone = template.content.cloneNode(true);
 
   clone.querySelector(".prayer-name").textContent = name;
   clone.querySelector(".prayer-time").textContent = time.time;
-  //   console.log(template.content);
   clone.querySelector(".prayer-span").textContent = time.period;
 
   timeContainer.appendChild(clone);
 }
 
+// TODO: handle count down upcoming prayer
+
+function updateDate(hijri, date) {
+  const formattedhijri = `${hijri.day} ${hijri.month.en}, ${hijri.year}`;
+  hijriDate.textContent = formattedhijri;
+  enDate.textContent = date;
+}
+
+async function renderLocationName(coords) {
+  const location = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?lat=${coords.latitude}&lon=${coords.longitude}&format=json`,
+  );
+
+  const data = await location.json();
+  const address = data.address;
+
+  const place =
+    address.city ||
+    address.town ||
+    address.village ||
+    address.state ||
+    "Unknown location";
+
+  const country = address.country || "";
+
+  console.log(data);
+  locationName.textContent = `${place}, ${country}`;
+}
+
 init();
-// console.log(getLocation());
